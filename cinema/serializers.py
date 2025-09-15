878dd755ac459c11bb678d49ac01788e76f61a2f
Iryna Hromska <irahromska@gmail.com>
2025-09-15 07:39:40 +0300

    fix

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
-
 from cinema.models import Movie
 
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=100, required=True)
+    title = serializers.CharField(max_length=255, required=True)
     description = serializers.CharField(required=True)
     duration = serializers.IntegerField(required=True)
 
